4a8643512ea97362d286821f5e4a3da11650ea38
MattWesterhaus <71035752+MattWesterhaus@users.noreply.github.com>
2022-12-12 11:02:55 -0600

    Updated the About Page

--- src/components/about.vue
+++ src/components/about.vue
@@ -28,32 +28,39 @@ export default {
         <div class="text-center">
             <table>
               <tr>
-                <th>Tool</th>
+                <th style="width:25%">Tool</th>
                 <th>How It Was Used</th>
               </tr>
               <tr>
-                <td>Vue.js  </td>
-                <td>Explain here</td>
+                <td>Vue JS  </td>
+                <td>Vue JS is a JavaScript framework that makes it much easier to make your website responsive. We used Vue within each of our buttons 
+                    to accomlish things like changing the page in the menu or accessing the database in submit/delete buttons. </td>
               </tr>
               <tr>
                 <td>Foundation</td>
-                <td>Explain here</td>
+                <td>Foundation is a web design framework that we used to format our pages more uniformily and more easily. It contains many premade css classes
+                    that we were able to take advantage of for the making of the front-end aspect of the website.
+                </td>
               </tr>
               <tr>
-                <td>Tool</td>
-                <td>Explain here</td>
+                <td>Nominatim API</td>
+                <td>The Nominatim API allows you to search for locations by name, or retrieve a location's name based on lat/long coordinates. We used this 
+                    in our search box to allow the user to input where to look on the map.
+                </td>
               </tr>
               <tr>
-                <td>Tool</td>
-                <td>Explain here</td>
+                <td>Leaflet</td>
+                <td>This API allows you to render interactive maps in your web page as well as overlay data on top of the map. 
+                    The map can be modified either by user interaction or by JavaScript commands. We used this to display the map on the main page</td>
               </tr>
               <tr>
-                <td>Tool</td>
-                <td>Explain here</td>
+                <td>St. Paul Crime</td>
+                <td>This is an api that we made earlier in the semester using public data on crime in St. Paul Minnesota. We used it to populate our map and table of 
+                    incident results with their filters. We also used it when deleting an incident or submitting a new one.</td>
               </tr>
 
             </table>
-
+            <!-- What else???-->
         </div>
            
     </div>
@@ -115,6 +122,7 @@ export default {
             After graduating in December I will start working at Optum in July 2023 in their Technology Development Program (TDP). I interned with them this 
             past summer and enjoyed it so I am super thrilled to be going back full time. 
         </p>
+        Here is my <a href="https://github.com/Matt-Westerhaus/Matt-Westerhaus"> Github</a>
     </div>
     <div class="cell small-6 large-6 center"><img src ="images\matt_image.jpg" id="float-center" alt="Picture of Matt"/></div> 
 </div>
